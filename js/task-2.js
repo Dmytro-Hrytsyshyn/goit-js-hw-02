4dc3f3f550ce6d0d2b7e5d80b7214a2d18d046fe
Nataliia <nataliiaqa8@gmail.com>
2024-06-22 16:32:26 +0200

    adada

--- js/task-2.js
+++ js/task-2.js
@@ -1,10 +1,10 @@
 function formatMessage(message, maxLength){
 
-    if(message <= maxLength){
+    if(message.length <= maxLength){
         return message;
 
     }else{
-            return message.slice(0, maxLength) + "..."
+            return message.slice(0, maxLength) + "...";
         }
     }
 
